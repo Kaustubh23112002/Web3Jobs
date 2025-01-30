9c78c6faf0fce284a8e5003797d1b79053baeda8
Kaustubh Bhoir <kaustubhbhoir173@gmail.com>
2025-01-30 11:28:08 +0530

    footer changes-1

--- frontend/src/components/CryptoTicker.jsx
+++ frontend/src/components/CryptoTicker.jsx
@@ -8,7 +8,7 @@ const CryptoTicker = () => {
     const fetchCryptoPrices = async () => {
       try {
         const response = await fetch(
-          "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false"
+          "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd"
         );
         const data = await response.json();
         setCryptoData(data);
